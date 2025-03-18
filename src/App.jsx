a9491770d1a310f26a4f42ceef31092676189162
sugamcr7 <sugamkarmakar338@gmail.com>
2025-03-18 16:00:22 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import Header from "./components/Layouts/Header/Header";
 import TopHeader from "./components/Layouts/Header/TopHeader";
 import Footer from "./components/Layouts/Footer/Footer";
+
+// Lazy-loaded components should be wrapped in Suspense
 const ContactUs = React.lazy(() => import("./components/Pages/Contact/ContactUs"));
-import Service from "./components/Pages/Service/Service";
-import About from "./components/Pages/AboutUs/About";
-import Industry from "./components/Pages/Industry/Industry";
-import Home from "./components/Pages/Home/Home";
-import Landing from "./components/Landing/Landing";
+const Service = React.lazy(() => import("./components/Pages/Service/Service"));
+const About = React.lazy(() => import("./components/Pages/AboutUs/About"));
+const Industry = React.lazy(() => import("./components/Pages/Industry/Industry"));
+const Home = React.lazy(() => import("./components/Pages/Home/Home"));
+const Landing = React.lazy(() => import("./components/Landing/Landing"));
 
 const AppContent = () => {
   const location = useLocation();
-  
   const hideHeaderFooterRoutes = ["/web-design-development-company"];
 
   return (
@@ -24,14 +25,17 @@ const AppContent = () => {
         </>
       )}
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<ContactUs />} />
-        <Route path="/service" element={<Service />} />
-        <Route path="/industry" element={<Industry />} />
-        <Route path="/web-design-development-company" element={<Landing />} />
-      </Routes>
+      {/* Wrap lazy-loaded components inside Suspense */}
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/industry" element={<Industry />} />
+          <Route path="/web-design-development-company" element={<Landing />} />
+        </Routes>
+      </Suspense>
 
       {!hideHeaderFooterRoutes.includes(location.pathname) && <Footer />}
     </>
